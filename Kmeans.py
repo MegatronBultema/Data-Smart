961a736b89ad19b4add1a5dcab3dbdcac494bda2
MegatronBultema <mrbkc.mxc@gmail.com>
2017-01-24 13:35:21 -0700

    Update Kmeans.py

--- Kmeans.py
+++ Kmeans.py
@@ -7,17 +7,10 @@ from sklearn.cluster import KMeans
 import sklearn.metrics as sm
 
 
-def read_csv(filename):
-    with open(filename, 'rb') as f:
-        reader= csv.reader(f)
-        your_list=map(tuple,reader)
-    
-    print your_list
-    print your_list[1]
-
-    return your_list
-
 def read_pivot(filename,sheetname):
+    """Read in a sheet from an excel file and create a pivot table as a Pandas DataFrame using the Offer # as the index and Customer Last Name as columns. 
+    Aggregate function is count non-zero and the fill value is 0. Function should be returning 1 but is returning 2, I think because it 
+    also counting the column number label. At the moment there is a bandaid to replace all instances of 2 with 1 but this needs to be addressed."""
     import pandas as pd
     import numpy as np
     f=pd.read_excel(filename, sheetname=sheetname)
@@ -29,6 +22,8 @@ def read_pivot(filename,sheetname):
     return piv
 
 def read_OI(filename, sheetname):
+    """Read in sheet from excel and create a Pandas DataFrame using the Offer # as the index)
+    """
     import pandas as pd
     import numpy as np
     oi=pd.read_excel(filename, sheetname=sheetname)
@@ -37,7 +32,9 @@ def read_OI(filename, sheetname):
     return oi
 
 def concat(oi,rp):
-    frames=(oi,rp)
+    """Concatinate the Offer Information DataFrame with the pivot table
+    """
+    frames=(oi,piv)
     merge=pd.concat(frames, axis=1)
     return merge
 
@@ -46,8 +43,12 @@ def trans(concat):
     trans=trans.transpose()
     return trans
 
-def kmeans(trans,nclust):
+def kmeans(data,nclust):
     from sklearn.cluster import KMeans
-    model=KMeans(n_clusters=nclust)
-    return (model.fit(trans),model.fit_transform(trans))
-    print model.labels_
+    model=KMeans(n_clusters=nclust).fit(data)
+    modeldist=model.transform(data)
+    md=pd.DataFrame(modeldist,columns=('Distance to Cluster0', 'Distance to Cluster1','Distance to Cluster2', 'Distance to Cluster3'))
+    data_out=data.copy()
+    data_out['Cluster Class']=pd.Series(model.labels_,index=data_out.index)
+    data_out=pd.concat([data_out, md.set_index(data_out.index[:len(md)])],axis=1)
+    
